23ef391df6cea018085839e1cfdd8d38a8900e35
Gemdas <Brennan.artzt@gmail.com>
2017-11-15 10:55:04 -0600

    models created

---
+++ models/reservation.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+	var Reservation = sequelize.define("reservation",{
+		time: {
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    	},
+    	groupName: {
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    		validate:{
+    			notEmpty: true
+    		}
+    	},
+    	playerCount:{
+    		type:DataTypes.INTEGER,
+    		defaultValue:1,
+    		validate:{
+    			isInt: true,
+    			max: 4,
+    			min: 1
+    		}	
+    	}
+    }
+    Reservation.associate=function(models){
+   		Reservation.belongsTo(models.user, {
+   			onDelete: "CASCADE",
+			foreignKey: {
+				allowNull: false
+   			}
+   		})
+    }
+    return Reservation;
+}
---
+++ models/user.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+	var User = sequelize.define("user", {
+		email: {
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    		unique:true,
+    		validate:{
+    			isEmail: true
+    		}
+    	},
+    	password:{
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    		validate:{
+    			min: [8]
+    		}
+    	},
+    	firstName: {
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    		validate:{
+    			isAlpha: true,
+    			notEmpty: true
+    		}
+    	},
+    	lastName: {
+    		type:DataTypes.STRING,
+    		allowNull:false,
+    		validate:{
+    			isAlpha: true,
+    			notEmpty: true
+    		}
+    	},
+    	isAdmin: {
+    		type:DataTypes.BOOLEAN,
+    		defaultValue:false
+    	}
+	});
+	return User;
+};
