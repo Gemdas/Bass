b080a70a5ad23f6c980f3b33ededf9a461035ed5
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-11-16 21:52:49 -0600

    passport.js

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
     	},
     	firstName: {
     		type:DataTypes.STRING,
-    		/*allowNull:false,*/
+    		allowNull:false,
     		validate:{
     			isAlpha: true,
     			notEmpty: true
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
     	},
     	lastName: {
     		type:DataTypes.STRING,
-    		/*allowNull:false,*/
+    		allowNull:false,
     		validate:{
     			isAlpha: true,
     			notEmpty: true
