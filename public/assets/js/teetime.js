70ff0e98d1e4ba590338fb17b23141611d600464
Sam Sadler <samsadler@Sams-MacBook-Pro.local>
2017-11-18 10:30:51 -0600

    adding form

--- public/assets/js/teetime.js
+++ public/assets/js/teetime.js
@@ -1,2 +1,21 @@
 // functionality for the front-end goes here
 // ajax requests go here
+
+ // when the user selects the form in the teesheet, a module should pop up
+ // the module will have buttons 1-4 to select how may players they will have in the group
+ // selecting submit will send an $.post to the database storing the teetime
+$(document).ready(function(){
+	$('#form').hide();
+
+	$('#submit').click(function(){
+		// $('#form').show();
+		$('#form').dialog({
+			
+			height: 'auto',
+			width: 'auto',
+			modal: true
+			
+		});
+
+	});
+});
