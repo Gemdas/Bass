3ed048da39619ed113ab7d1fb42847b7fbea5cc4
Setu Biswas <setubiswas@cpe-172-100-3-28.twcny.res.rr.com>
2017-11-28 15:33:41 -0600

    fixed redirect problems

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,11 +1,31 @@
+var moment = require("moment");
 module.exports = function(app, passport) {
  
+// app.post('/users', passport.authenticate('local-signin', {
+//             successRedirect: '/teetime/'+moment().format("dddd"),
+ 
+//             failureRedirect: '/fail'
+//         }
+ 
+//     ));
+ 
+// }
+
 app.post('/users', passport.authenticate('local-signin', {
-            successRedirect: '/teetime',
+            successRedirect: '/pass',
  
             failureRedirect: '/fail'
         }
  
     ));
  
+
+app.get('/pass', function(req, res){
+	res.json(true);
+});
+
+app.get('/fail', function(req, res){
+	res.json(false);
+});
+
 }
