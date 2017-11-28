b32d12c8dad5d84071d667cc2e76d46e42824fe8
gemdas <brennan.artzt@gmail.com>
2017-11-28 17:08:27 -0600

    email works

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -2,19 +2,20 @@ var nodemailer = require('nodemailer');
 var db = require('../models');
 var url = require("url")
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'bassteetime@gmail.com',
-    pass: 'frankenstein'
-  }
+	service: 'gmail',
+	auth: {
+		user: 'bassteetime@gmail.com',
+		pass: 'frankenstein'
+	}
 });
 module.exports = function (app) {
 	app.post("/invite", function(req, res){
 		var email=req.body.email;
+		var server= req.body.server;
 		if(email.indexOf("@")===-1){
 			res.json({
-				message:"not valid email"
-				isSuccess:false,
+				message:"not valid email",
+				isSuccess:false
 			})
 		}
 		db.user.findOne({
@@ -33,8 +34,15 @@ module.exports = function (app) {
 					from: 'bassteetime@gmail.com',
 					to: email,
 					subject: 'New User Invite',
-					text: "localhost:8080/newUser?email="+email
+					text: server+"/newUser?email="+email
 				};
+				transporter.sendMail(mailOptions, function(error, info){
+					if (error) {
+						console.log(error);
+					} else {
+						console.log('Email sent: ' + info.response);
+					}
+				});
 			}
 		})
 	})
